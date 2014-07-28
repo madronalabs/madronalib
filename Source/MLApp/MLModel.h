6677d18f749c3d67094b5d5914e7fdfeed4e3586
Randy Jones <randy@madronalabs.com>
2014-07-28 12:26:48 -0700

    made MLProperty its own module

--- Source/MLApp/MLModel.h
+++ Source/MLApp/MLModel.h
@@ -6,157 +6,10 @@
 #ifndef __ML_MODEL__
 #define __ML_MODEL__
 
-#include <string>
-#include "MLSignal.h"
-#include "MLSymbol.h"
-#include "MLDebug.h"
+#include "MLProperty.h"
 
-// MLProperty: a modifiable property. Properties have three types: float, string, and signal.
-// Properties start out with undefined type. Once a type is assigned, properties cannot change type.
-
-class MLProperty
-{
-public:
-	enum eType
-	{
-		kUndefinedProperty	= 0,
-		kFloatProperty	= 1,
-		kStringProperty = 2,
-		kSignalProperty = 3
-	};
-    
-	MLProperty();
-	MLProperty(const MLProperty& other);
-	MLProperty& operator= (const MLProperty & other);
-	MLProperty(float v);
-	MLProperty(const std::string& s);
-	MLProperty(const MLSignal& s);
-	~MLProperty();
-    
-	float getFloatValue() const;
-	const std::string* getStringValue() const;
-	const MLSignal* getSignalValue() const;
-    
-	void setValue(float v);
-	void setValue(const std::string& v);
-	void setValue(const MLSignal& v);
-	
-	bool operator== (const MLProperty& b) const;
-	bool operator!= (const MLProperty& b) const;
-	eType getType() const { return mType; }
-	
-	bool operator<< (const MLProperty& b) const;
-	
-private:
-	eType mType;
-	union
-	{
-		float mFloatVal;
-		std::string* mpStringVal;
-		MLSignal* mpSignalVal;
-	}   mVal;
-};
-
-std::ostream& operator<< (std::ostream& out, const MLProperty & r);
-
-class MLPropertyListener;
-class MLPropertyModifier;
-
-// a Set of Properties. Property names are stored here.
-
-class MLPropertySet
-{
-friend class MLPropertyModifier;
-public:
-	MLPropertySet();
-	virtual ~MLPropertySet();
-
-	inline const MLProperty& getProperty(MLSymbol p) { return mProperties[p]; }
-	inline float getFloatProperty(MLSymbol p) { return mProperties[p].getFloatValue(); }
-	inline const std::string* getStringProperty(MLSymbol p) { return mProperties[p].getStringValue(); }
-	inline const MLSignal* getSignalProperty(MLSymbol p) { return mProperties[p].getSignalValue(); }
-
-	void addPropertyListener(MLPropertyListener* pL);
-	void removePropertyListener(MLPropertyListener* pToRemove);
-    void broadcastAllProperties();
-
-protected:
-    void setProperty(MLSymbol p, float v);
-    void setProperty(MLSymbol p, const std::string& v);
-    void setProperty(MLSymbol p, const MLSignal& v);
-	
-private:
-	std::map<MLSymbol, MLProperty> mProperties;
-	std::list<MLPropertyListener*> mpListeners;
-	void broadcastProperty(MLSymbol p);
-};
-
-// MLPropertyListeners are notified when a Property of an MLPropertySet changes. They do something in
-// response by overriding doPropertyChangeAction().
-
-class MLPropertyListener
-{
-friend class MLPropertySet;
-public:
-	MLPropertyListener(MLPropertySet* m) : mpPropertyOwner(m)
-    {
-        mpPropertyOwner->addPropertyListener(this);
-    }
-	virtual ~MLPropertyListener() {}
-    
-	virtual void doPropertyChangeAction(MLSymbol param, const MLProperty & newVal) = 0;
-	
-	// do an action for any params with changed values.
-	void updateChangedProperties();
-	
-	// force an update of all params.
-	void updateAllProperties();
-    
-protected:
-    // mark one property as changed.
-	void propertyChanged(MLSymbol p);
-    
-    // called by property owners if they going to be deleted
-    void ownerClosing();
-    
-	// represent the state of a property relative to updates.
-	class PropertyState
-	{
-	public:
-		PropertyState() :
-        mChangedSinceUpdate(true)
-        {};
-        
-		~PropertyState() {}
-		
-		bool mChangedSinceUpdate;
-		MLProperty mValue;
-	};
-    
-	std::map<MLSymbol, PropertyState> mPropertyStates;
-	MLPropertySet* mpPropertyOwner;
-};
-
-// an MLPropertyModifier can request that PropertySets make changes to Properties.
-// use, for example, to control a Model from a UI or recall it to a saved state.
-
-class MLPropertyModifier
-{
-public:
-	MLPropertyModifier(MLPropertySet* m) : mpPropertyOwner(m) {}
-	virtual ~MLPropertyModifier() {}
-    void requestPropertyChange(MLSymbol p, float v);
-    void requestPropertyChange(MLSymbol p, const std::string& v);
-    void requestPropertyChange(MLSymbol p, const MLSignal& v);
-
-private:
-    MLPropertySet* mpPropertyOwner;
-};
-
-// Finally, a Model is a kind of PropertySet that is also its own PropertyListener.
+// an MLModel is a kind of PropertySet that is also its own PropertyListener.
 // Override doPropertyChangeAction to propagate any changes from Properties to the core logic.
-
-// TODO write a Timer class. juce::Timer is the only reason Juce is needed here. temporary.
 
 class MLModel : public MLPropertySet, public MLPropertyListener, public juce::Timer
 {
@@ -164,7 +17,7 @@ public:
 	MLModel();
 	virtual ~MLModel();
     void timerCallback();
-	virtual void doPropertyChangeAction(MLSymbol param, const MLProperty & newVal) = 0;
+    // TODO write a Timer class. juce::Timer is the only reason Juce is needed here. temporary.
 };
 
 #endif // __ML_MODEL__
