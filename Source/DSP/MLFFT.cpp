e62a35b0a8b198034b39f4a711d21c65ca000a08
Randy Jones <randy@madronalabs.com>
2015-03-16 13:30:50 -0700

    add placeholder FFT code

---
+++ Source/DSP/MLFFT.cpp
@@ -0,0 +1,9 @@
+//
+//  MLFFT.cpp
+//  Soundplane
+//
+//  Created by Randy Jones on 3/14/15.
+//
+//
+
+#include "MLFFT.h"
---
+++ Source/DSP/MLFFT.h
@@ -0,0 +1,14 @@
+//
+//  MLFFT.h
+//  Soundplane
+//
+//  Created by Randy Jones on 3/14/15.
+//
+//
+
+#ifndef __Soundplane__MLFFT__
+#define __Soundplane__MLFFT__
+
+#include <stdio.h>
+
+#endif /* defined(__Soundplane__MLFFT__) */
