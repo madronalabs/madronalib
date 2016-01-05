64b9684dcf4dc720f09d362a8e5a2b3278e079cd
Randy Jones <randy@madronalabs.com>
2016-01-05 12:06:04 -0800

    Projections

--- source/core/MLProjection.cpp
+++ source/core/MLProjection.cpp
@@ -7,3 +7,16 @@
 //
 
 #include "MLProjection.h"
+
+namespace ml
+{
+	namespace projections
+	{
+		// constant projections 		
+		const Projection linear( [](float x){return x;} );
+		const Projection flip( [](float x){return 1 - x;} );
+		const Projection clip( [](float x){return clamp(x, 0.f, 1.f);} );
+		const Projection smoothstep( [](float x){return 3*x*x - 2*x*x*x;} );
+		const Projection bell( [](float x){float px = x*2 - 1; return powf(2.f, -(10.f*px*px));} );
+	}
+}
--- source/core/MLProjection.h
+++ source/core/MLProjection.h
@@ -9,6 +9,90 @@
 #ifndef __Virta__MLProjection__
 #define __Virta__MLProjection__
 
-#include <stdio.h>
+#include <functional>
+#include "MLDSP.h"
+
+// TODO replace MLRange with this stuff
+namespace ml
+{
+	// Intervals
+	
+	class Interval
+	{
+	public:
+		Interval(float x1, float x2) : mX1(x1), mX2(x2) {}
+		Interval(int x1, int x2) : mX1(x1), mX2(x2) {}
+		float mX1, mX2;
+	};
+	
+	inline bool within(float f, const Interval m) { return (f >= m.mX1)&&(f < m.mX2); }
+	
+	// Projections
+	
+	typedef std::function<float(float)> Projection;
+	
+	inline Projection compose(Projection a, Projection b)
+	{
+		return( [=](float x){return a(b(x));} );
+	}
+	
+	// useful projections defined on (0, 1)
+	namespace projections
+	{
+		// constant projections 		
+		extern const Projection linear;
+		extern const Projection flip;
+		extern const Projection clip;
+		extern const Projection smoothstep;
+		extern const Projection bell;
+		
+		// functions returning projections
+		
+		// returns a projection that will be logarithmic when scaled and offset to (a, b).
+		// works for positive a, b with a < b only.
+		inline Projection log(Interval m)
+		{
+			return( [=](float x){float a = m.mX1; float b = m.mX2; return a*(powf((b/a), x) - 1)/(b - a);} );
+		}
+		
+		// the inverse of the log projection.
+		// works for positive a, b with a < b only.
+		inline Projection exp(Interval m)
+		{
+			return( [=](float x){float a = m.mX1; float b = m.mX2; return logf((x*(b - a) + a)/a)/logf(b/a);} );
+		}
+	}
+	
+	// IntervalProjection: a functor that maps one interval to another with
+	// an optional mapping projection defined on (0, 1)->(0,1).
+	
+	class IntervalProjection
+	{
+	public:
+		// define a projection from interval a to interval b.
+		// projection c is defined on [0, 1)->[0, 1) and can add clipping or nonlinear projections. 
+		IntervalProjection(const Interval a, const Interval b, Projection c = projections::linear) : mA(a), mB(b), mMappingProjection(c) { build(); }
+		
+		inline float operator()(float f) const 
+		{
+			return mMappingProjection(f*mScaleA + mOffsetA)*mScaleB + mOffsetB;
+		}	
+		
+	private:
+		const Interval mA, mB;
+		const Projection mMappingProjection;
+		float mScaleA, mOffsetA, mScaleB, mOffsetB;
+		
+		void build()
+		{
+			// project interval a to interval (0,1)
+			mScaleA = 1/(mA.mX2 - mA.mX1);
+			mOffsetA = (-mA.mX1) / (mA.mX2 - mA.mX1);
+			// project interval (0, 1) to interval b
+			mScaleB = (mB.mX2 - mB.mX1);
+			mOffsetB = mB.mX1;
+		}
+	};
+}
 
 #endif /* defined(__Virta__MLProjection__) */
