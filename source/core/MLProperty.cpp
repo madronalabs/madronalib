843146409702cc18166295d7a0b0f3edb595252c
Randy Jones <randy@madronalabs.com>
2016-07-28 16:20:20 -0700

    moved MLProperty to core.

---
+++ source/core/MLProperty.cpp
@@ -0,0 +1,171 @@
+
+// MadronaLib: a C++ framework for DSP applications.
+// Copyright (c) 2013 Madrona Labs LLC. http://www.madronalabs.com
+// Distributed under the MIT license: http://madrona-labs.mit-license.org/
+
+#include "MLProperty.h"
+
+#pragma mark MLProperty
+
+const MLSignal MLProperty::nullSignal;
+
+MLProperty::MLProperty() :
+	mType(kUndefinedProperty),
+	mFloatVal(0)
+{
+}
+
+MLProperty::MLProperty(const MLProperty& other) :
+	mType(other.getType()),
+	mFloatVal(0)
+{
+	switch(mType)
+	{
+		case MLProperty::kFloatProperty:
+			mFloatVal = other.getFloatValue();
+			break;
+		case MLProperty::kTextProperty:
+			mTextVal = other.getTextValue();
+			break;
+		case MLProperty::kSignalProperty:
+			mSignalVal = other.getSignalValue();
+			break;
+		default:
+			break;
+	}
+}
+
+MLProperty& MLProperty::operator= (const MLProperty& other)
+{
+	// copy data in place if possible
+	mType = other.getType();
+	switch(mType)
+	{
+		case MLProperty::kFloatProperty:
+			mFloatVal = other.getFloatValue();
+			break;
+		case MLProperty::kTextProperty:
+			mTextVal = other.getTextValue();
+			break;
+		case MLProperty::kSignalProperty:
+			// MLSignal handles copy-in-place when possible
+			mSignalVal = other.getSignalValue();
+			break;
+		default:
+			break;
+	}
+
+	return *this;
+}
+
+MLProperty::MLProperty(float v) :
+	mType(kFloatProperty)
+{
+	mFloatVal = v;
+}
+
+MLProperty::MLProperty(const ml::Text& t) :
+	mType(kTextProperty)
+{
+	mTextVal = t;
+}
+
+MLProperty::MLProperty(const MLSignal& s) :
+	mType(kSignalProperty)
+{
+	mSignalVal = s;
+}
+
+MLProperty::~MLProperty()
+{
+}
+
+const float MLProperty::getFloatValue() const
+{
+	static const float nullFloat = 0.f;
+	return (mType == kFloatProperty) ? mFloatVal : nullFloat;
+}
+
+const ml::Text MLProperty::getTextValue() const
+{
+	return (mType == kTextProperty) ? (mTextVal) : ml::Text();
+}
+
+const MLSignal& MLProperty::getSignalValue() const
+{
+	return (mType == kSignalProperty) ? (mSignalVal) : nullSignal;
+}
+
+void MLProperty::setValue(const float& v)
+{
+	mType = kFloatProperty;
+	mFloatVal = v;
+}
+
+void MLProperty::setValue(const ml::Text& v)
+{
+	mType = kTextProperty;
+	mTextVal = v;
+}
+
+void MLProperty::setValue(const MLSignal& v)
+{
+	mType = kSignalProperty;
+	mSignalVal = v;
+}
+
+void MLProperty::setValue(const MLProperty& v)
+{
+	*this = v;
+}
+
+bool MLProperty::operator== (const MLProperty& b) const
+{
+	bool r = false;
+	if(mType == b.getType())
+	{
+		switch(mType)
+		{
+			case kUndefinedProperty:
+				r = true;
+				break;
+			case kFloatProperty:
+				r = (getFloatValue() == b.getFloatValue());
+				break;
+			case kTextProperty:
+				r = (getTextValue() == b.getTextValue());
+				break;
+			case kSignalProperty:
+				r = (getSignalValue() == b.getSignalValue());
+				break;
+		}
+	}
+	return r;
+}
+
+bool MLProperty::operator!= (const MLProperty& b) const
+{
+	return !operator==(b);
+}
+
+#pragma mark MLProperty utilities
+
+std::ostream& operator<< (std::ostream& out, const MLProperty & r)
+{
+	switch(r.getType())
+	{
+		case MLProperty::kUndefinedProperty:
+			out << "[undefined]";
+			break;
+		case MLProperty::kFloatProperty:
+			out << r.getFloatValue();
+			break;
+		case MLProperty::kTextProperty:
+			out << r.getTextValue();
+			break;
+		case MLProperty::kSignalProperty:
+			out << r.getSignalValue();
+            break;
+	}
+	return out;
+}
---
+++ source/core/MLProperty.h
@@ -0,0 +1,76 @@
+
+// MadronaLib: a C++ framework for DSP applications.
+// Copyright (c) 2013 Madrona Labs LLC. http://www.madronalabs.com
+// Distributed under the MIT license: http://madrona-labs.mit-license.org/
+
+#pragma once
+
+#include <string>
+#include <list>
+#include <map>
+#include "MLSignal.h"
+#include "MLSymbol.h"
+#include "MLText.h"
+
+// MLProperty: a modifiable property. Properties have four types: undefined, float, text, and signal.
+
+class MLProperty
+{
+public:
+	static const MLSignal nullSignal;
+
+	enum Type
+	{
+		kUndefinedProperty	= 0,
+		kFloatProperty	= 1,
+		kTextProperty = 2,
+		kSignalProperty = 3
+	};
+
+	MLProperty();
+	MLProperty(const MLProperty& other);
+	MLProperty& operator= (const MLProperty & other);
+	MLProperty(float v);
+	MLProperty(const ml::Text& t); 
+	MLProperty(const MLSignal& s);
+
+	// signal type constructor via initializer_list
+	explicit MLProperty (std::initializer_list<float> values)
+	{
+		*this = MLProperty(MLSignal(values));
+	}
+
+	~MLProperty();
+    
+	const float getFloatValue() const;
+	const ml::Text getTextValue() const;
+	const MLSignal& getSignalValue() const;
+    
+	// For each type of property, a setValue method must exist
+	// to set the value of the property to that of the argument.
+	//
+	// For each type of property, if the size of the argument is equal to the
+	// size of the current value, the value must be modified in place.
+	// This guarantee keeps DSP graphs from allocating memory as they run.
+	void setValue(const MLProperty& v);
+	void setValue(const float& v);
+	void setValue(const ml::Text& v);
+	void setValue(const MLSignal& v);
+	
+	bool operator== (const MLProperty& b) const;
+	bool operator!= (const MLProperty& b) const;
+	Type getType() const { return mType; }
+	
+	bool operator<< (const MLProperty& b) const;
+	
+private:
+	// TODO reduce storage requirements-- this is a minimal-code start
+	Type mType;
+	float mFloatVal;
+	ml::Text mTextVal;
+	MLSignal mSignalVal;
+};
+
+// utilities
+
+std::ostream& operator<< (std::ostream& out, const MLProperty & r);
