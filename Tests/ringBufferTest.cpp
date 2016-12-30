d2e8d982a738738e0387851378eb40196629b61d
Randy Jones <randy@madronalabs.com>
2016-12-30 12:15:13 -0800

    fixed tests and added proper RingBuffer test

--- Tests/ringBufferTest.cpp
+++ Tests/ringBufferTest.cpp
@@ -19,7 +19,7 @@ public:
 	int mTime;
 };
 
-const int kMaxControlEventsPerBlock = 64; // must be power of two!
+const int kTestBufferSize = 64; // must be power of two!
 
 TestEvent::TestEvent() :
 mValue1(0.),
@@ -40,27 +40,25 @@ TEST_CASE("madronalib/core/ringbuffer", "[ringbuffer]")
 	PaUtilRingBuffer eventQueue;
 	
 	// setup event queue
-	eventData.resize(kMaxControlEventsPerBlock);
-	PaUtil_InitializeRingBuffer( &eventQueue, sizeof(TestEvent), kMaxControlEventsPerBlock, &(eventData[0]) );
+	eventData.resize(kTestBufferSize);
+	PaUtil_InitializeRingBuffer( &eventQueue, sizeof(TestEvent), kTestBufferSize, &(eventData[0]) );
 
-	// write and read some events
-	int padding = kMaxControlEventsPerBlock - 10;
+	// write and read some events to get near the end of the physical buffer
+	int padding = kTestBufferSize - 10;
 	for(int i=0; i<padding; ++i)
 	{
 		TestEvent e(i, 23.0);
 		PaUtil_WriteRingBuffer( &eventQueue, &e, 1 );
-	}
-	
+	}	
 	while(PaUtil_GetRingBufferReadAvailable(&eventQueue))
 	{
 		TestEvent e;
 		PaUtil_ReadRingBuffer( &eventQueue, &e, 1 );
-		std::cout << e.mTime << "\n";
 	}
 	
 	// write some events wrapped around buffer end 
-	int elems = 20;
-	for(int i=0; i<elems; ++i)
+	const int testElems = 20;
+	for(int i=0; i<testElems; ++i)
 	{
 		int time = i;
 		float v1 = (time > 8) ? (36.2 - i + 0.5f) : 99.;
@@ -68,29 +66,38 @@ TEST_CASE("madronalib/core/ringbuffer", "[ringbuffer]")
 		PaUtil_WriteRingBuffer( &eventQueue, &e, 1 );
 	}
 	
-	AvailableElementsVector<TestEvent> v(&eventQueue);
-	
-	std::cout << "size: " << v.size() << "\n";
-	
-	AvailableElementsVector<TestEvent>::iterator i1 = v.begin();
-	AvailableElementsVector<TestEvent>::iterator i2 = i1;
+	// test sorting a divided vector of current elements through RingBufferElementsVector
+	RingBufferElementsVector<TestEvent> v(&eventQueue);
+	RingBufferElementsVector<TestEvent>::iterator i1 = v.begin();
+	RingBufferElementsVector<TestEvent>::iterator i2 = i1;
 	i1 += 1;
 	i2 += 5;
-
 	std::iter_swap(i1, i2);
-	
-	std::cout << "---------------------------\n\n";
-	for(auto it = v.begin(); it != v.end(); it++)
-	{	
-		std::cout << (*it).mTime << " : " << (*it).mValue1 << "\n";
-	}
-	
 	std::sort(v.begin(), v.end(), [](TestEvent a, TestEvent b){ return a.mValue1 < b.mValue1; });
- 
-	std::cout << "---------------------------\n\n";
-	for(auto it = v.begin(); it != v.end(); it++)
+
+	// read out elements one at a time and check for sorted order
+	bool sorted = true;
+	float valueA = -MAXFLOAT;
+	float valueB;
+	bool empty = false;
+	int count = 0;
+	while(!empty)
 	{	
-		std::cout << (*it).mTime << " : " << (*it).mValue1 << "\n";
+		TestEvent e;
+		empty = !(PaUtil_ReadRingBuffer( &eventQueue, &e, 1 ));
+		if(!empty) 
+		{			
+			valueB = e.mValue1;		
+			if(valueA > valueB) 
+			{
+				sorted = false;
+				std::cout << "not sorted! (" << valueA << " > " << valueB << ") \n";
+			}
+			valueA = valueB;
+			count++;
+		}
 	}
+	REQUIRE(sorted);	
+	REQUIRE(count == testElems);
 }
 
