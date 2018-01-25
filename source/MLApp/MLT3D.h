e42dc9d266569f21a340703a2b4bc02c53c016ed
Randy Jones <randy@FreshBook.local>
2018-01-25 08:06:50 -0800

    change t3d UDP port numbers

--- source/MLApp/MLT3D.h
+++ source/MLApp/MLT3D.h
@@ -6,11 +6,11 @@
 #pragma once
 
 // default port for t3d plugin communication. Plugins may be receiving on different ports.
-const int kDefaultUDPPort = 3125;
+const int kDefaultUDPPort = 3123;
 
 // maximum number of ports from kDefaultUDPPort to (kDefaultUDPPort + kNumUDPPorts - 1)
 const int kNumUDPPorts = 16;
 
 // Soundplane app input port for Kyma and other config messages
-const int kDefaultUDPReceivePort = 3124;
+const int kDefaultUDPReceivePort = 3122;
 
