15847a60dc1e00422b0e6a5126c952d7170ddbf6
Randy Jones <randy@FreshBook.local>
2018-07-16 14:22:48 -0700

    add Queue::clear()

--- source/core/MLQueue.h
+++ source/core/MLQueue.h
@@ -15,77 +15,83 @@
 #include <iterator>
 
 namespace ml {
-    
-template<typename Element>
-class Queue final
-{
-public:
-    Queue(size_t size)
-    {
-        mData.resize(size + 1);
-    }
-    
-    ~Queue(){}
-    
-    bool push(const Element& item)
-    {
-        const auto currentWriteIndex = mWriteIndex.load(std::memory_order_relaxed);
-        const auto nextWriteIndex = increment(currentWriteIndex);
-        if(nextWriteIndex != mReadIndex.load(std::memory_order_acquire))
-        {
-            mData[currentWriteIndex] = item;
-            mWriteIndex.store(nextWriteIndex, std::memory_order_release);
-            return true;
-        }
-        return false;
-    }
-    
-    bool pop(Element& item)
-    {
-        const auto currentReadIndex = mReadIndex.load(std::memory_order_relaxed);
-        if(currentReadIndex == mWriteIndex.load(std::memory_order_acquire))
-        {
-            return false; // empty queue
-        }
-        item = mData[currentReadIndex];
-        mReadIndex.store(increment(currentReadIndex), std::memory_order_release);
-        return true;
-    }
-    
-    size_t elementsAvailable() const
-    {
-        return (mWriteIndex.load(std::memory_order_acquire) - mReadIndex.load(std::memory_order_relaxed))%mData.size();
-    }
-  
-    // useful for reading elements while a criteria is met. Can be used like
-    // while queue.elementsAvailable() && q.peek().mTime < 100 { elem = q.pop() ... }
-    const Element& peek() const
-    {
-        const auto currentReadIndex = mReadIndex.load(std::memory_order_relaxed);
-        return mData[currentReadIndex];
-    }
-    
-    bool wasEmpty() const
-    {
-        return (mWriteIndex.load() == mReadIndex.load());
-    }
-    
-    bool wasFull() const
-    {
-        const auto nextWriteIndex = increment(mWriteIndex.load());
-        return (nextWriteIndex == mReadIndex.load());
-    }
-    
-private:
-    size_t increment(size_t idx) const
-    {
-        return (idx + 1) % (mData.size());
-    }
-    
-    std::vector<Element> mData;
-    std::atomic<size_t> mWriteIndex{0};
-    std::atomic<size_t> mReadIndex{0};
-
-};
+	
+	template<typename Element>
+	class Queue final
+	{
+	public:
+		Queue(size_t size)
+		{
+			mData.resize(size + 1);
+		}
+		
+		~Queue(){}
+		
+		bool push(const Element& item)
+		{
+			const auto currentWriteIndex = mWriteIndex.load(std::memory_order_relaxed);
+			const auto nextWriteIndex = increment(currentWriteIndex);
+			if(nextWriteIndex != mReadIndex.load(std::memory_order_acquire))
+			{
+				mData[currentWriteIndex] = item;
+				mWriteIndex.store(nextWriteIndex, std::memory_order_release);
+				return true;
+			}
+			return false;
+		}
+		
+		bool pop(Element& item)
+		{
+			const auto currentReadIndex = mReadIndex.load(std::memory_order_relaxed);
+			if(currentReadIndex == mWriteIndex.load(std::memory_order_acquire))
+			{
+				return false; // empty queue
+			}
+			item = mData[currentReadIndex];
+			mReadIndex.store(increment(currentReadIndex), std::memory_order_release);
+			return true;
+		}
+		
+		void clear()
+		{
+			Element dummy;
+			while(elementsAvailable()) pop(dummy);
+		}
+		
+		size_t elementsAvailable() const
+		{
+			return (mWriteIndex.load(std::memory_order_acquire) - mReadIndex.load(std::memory_order_relaxed))%mData.size();
+		}
+		
+		// useful for reading elements while a criteria is met. Can be used like
+		// while queue.elementsAvailable() && q.peek().mTime < 100 { elem = q.pop() ... }
+		const Element& peek() const
+		{
+			const auto currentReadIndex = mReadIndex.load(std::memory_order_relaxed);
+			return mData[currentReadIndex];
+		}
+		
+		bool wasEmpty() const
+		{
+			return (mWriteIndex.load() == mReadIndex.load());
+		}
+		
+		bool wasFull() const
+		{
+			const auto nextWriteIndex = increment(mWriteIndex.load());
+			return (nextWriteIndex == mReadIndex.load());
+		}
+		
+	private:
+		size_t increment(size_t idx) const
+		{
+			return (idx + 1) % (mData.size());
+		}
+		
+		std::vector<Element> mData;
+		std::atomic<size_t> mWriteIndex{0};
+		std::atomic<size_t> mReadIndex{0};
+		
+	};
 };
 
