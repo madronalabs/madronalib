7c58b8e634579a11de8e7bd2d0b90f4eac94949b
Randy Jones <randy@madronalabs.com>
2019-06-11 10:17:58 -0700

    rename

---
+++ include/mldsp.h
@@ -0,0 +1,18 @@
+
+// madronalib: a C++ framework for DSP applications.
+// Copyright (c) 2015-2019 Madrona Labs LLC. http://www.madronalabs.com
+// Distributed under the MIT license: http://madrona-labs.mit-license.org/
+
+#pragma once
+
+#include "MLDSPOps.h"
+#include "MLDSPFilters.h"
+#include "MLDSPGens.h"
+#include "MLDSPBuffer.h"
+#include "MLDSPFunctional.h"
+#include "MLDSPUtils.h"
+#include "MLProjection.h"
+#include "MLRatio.h"
+
+// TODO replace when loading code is updated #include "DSP/MLScale.h"
+
