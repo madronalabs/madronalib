b8793f3bde4168bfa18c64eab1938640eefa194e
Randy Jones <randy@madronalabs.com>
2019-09-20 14:39:10 -0700

    cleaned up example code by adding a header for RtAudio examples

--- examples/source/reverb.cpp
+++ examples/source/reverb.cpp
@@ -1,18 +1,13 @@
 // example of RtAudio wrapping low-level madronalib DSP code.
-// The reverb in this example is the Aaltoverb algorithm (madronalabs.com/products/Aaltoverb) without the tone control.
+// The reverb in this example is the Aaltoverb algorithm (madronalabs.com/products/Aaltoverb) without the tone control and some filtering.
 
-#include "mldsp.h"
-#include "RtAudio.h"
+#include "RtAudioExample.h"
 
 using namespace ml;
 
 constexpr int kInputChannels = 2;
 constexpr int kOutputChannels = 2;
 constexpr int kSampleRate = 44100;
-constexpr int kMaxProcessBlockFrames = 4096;
-
-// the VectorProcessBuffer buffers input from the RtAudio process routine and calls our process function.
-VectorProcessBuffer<kInputChannels, kOutputChannels, kMaxProcessBlockFrames> processBuffer;
 
 // log projection for decay parameter
 constexpr float kDecayLo = 0.8, kDecayHi = 20;
@@ -23,11 +18,9 @@ LinearGlide mSmoothFeedback;
 LinearGlide mSmoothDelay;
 
 // reverb machinery
-Hipass mHipassL, mHipassR;
 Allpass<PitchbendableDelay> mAp1, mAp2, mAp3, mAp4;
 Allpass<PitchbendableDelay> mAp5, mAp6, mAp7, mAp8, mAp9, mAp10;
 PitchbendableDelay mDelayL, mDelayR;
-OnePole mLp2, mLp3;
 
 // feedback storage
 DSPVector mvFeedbackL, mvFeedbackR;
@@ -60,10 +53,6 @@ void initializeReverb()
   mAp10.setMaxDelayInSamples(10000.f);
   mDelayL.setMaxDelayInSamples(3500.f);
   mDelayR.setMaxDelayInSamples(3500.f);
-
-  // set fixed filter coefficients
-  mLp2.mCoeffs = mLp3.mCoeffs = OnePole::coeffs(20000.f/kSampleRate);
-  mHipassL.mCoeffs = mHipassR.mCoeffs = Hipass::coeffs(20.f/kSampleRate, 1.414f);
 }
 
 // do filter processing in DSPVector-sized chunks.
@@ -81,7 +70,7 @@ DSPVectorArray<2> processVectors(const DSPVectorArray<2>& inputVectors)
   float decayIterations = decayTime/(sizeU*0.5);
   float feedback = (decayU < 1.0f) ? powf(RT60const, 1.0f/decayIterations) : 1.0f;
 
-  // generate smoothed delay and feedback vectors
+  // generate smoothed delay time and feedback gain vectors
   DSPVector vSmoothDelay = mSmoothDelay(sizeU*2.0f);
   DSPVector vSmoothFeedback = mSmoothFeedback(feedback);
 
@@ -98,7 +87,6 @@ DSPVectorArray<2> processVectors(const DSPVectorArray<2>& inputVectors)
   DSPVector vt9 = max(DSPVector(0.111*sr*vSmoothDelay), vMin);
   DSPVector vt10 = max(DSPVector(0.096*sr*vSmoothDelay), vMin);
 
-
   // sum stereo inputs and diffuse with allpass filters
   DSPVector monoInput = (inputVectors.constRow(0) + inputVectors.constRow(1));
   DSPVector diffusedInput = mAp4(mAp3(mAp2(mAp1(monoInput, vt1), vt2), vt3), vt4);
@@ -108,136 +96,22 @@ DSPVectorArray<2> processVectors(const DSPVectorArray<2>& inputVectors)
   DSPVector vDelayTimeR = max(vSmoothDelay*DSPVector(0.0371*sr) - vMin, DSPVector(0.f));
 
   // sum diffused input with feedback, and apply late diffusion
-  DSPVector vTapL = mHipassR(mAp7(mAp5(diffusedInput + mDelayL(mvFeedbackL, vDelayTimeL), vt5), vt7));
-  DSPVector vTapR = mHipassL(mAp8(mAp6(diffusedInput + mDelayR(mvFeedbackR, vDelayTimeR), vt6), vt8));
+  DSPVector vTapL = mAp7(mAp5(diffusedInput + mDelayL(mvFeedbackL, vDelayTimeL), vt5), vt7);
+  DSPVector vTapR = mAp8(mAp6(diffusedInput + mDelayR(mvFeedbackR, vDelayTimeR), vt6), vt8);
 
-  // get lowpass filter coeffs (passthru when decay = infinite)
-  if(feedback == 1.0f)
-  {
-    mLp2.mCoeffs = mLp3.mCoeffs = OnePole::passthru();
-  }
-  else
-  {
-    mLp2.mCoeffs = mLp3.mCoeffs = OnePole::coeffs(20000.f*(decayU*decayU)/sr);
-  }
+  mvFeedbackR = mAp9(vTapL, vt9)*vSmoothFeedback;
+  mvFeedbackL = mAp10(vTapR, vt10)*vSmoothFeedback;
 
-  // cross over late diffusion taps to feedback with lowpass filters
-  mvFeedbackR = (mLp2(mAp9(vTapL, vt9))) * vSmoothFeedback;
-  mvFeedbackL = (mLp3(mAp10(vTapR, vt10))) * vSmoothFeedback;
-
-  auto stereoOutput = append(vTapL, vTapR);
-  return stereoOutput;
-}
-
-// adapt the RtAudio process routine to a madronalib function operating on DSPBuffers.
-int callProcessVectorsBuffered( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
-          double /*streamTime*/, RtAudioStreamStatus status , void *data )
-{
-  const float *pInputBuffer = reinterpret_cast<const float *>(inputBuffer);
-  float *pOutputBuffer = reinterpret_cast<float *>(outputBuffer);
-
-  if ( status ) std::cout << "Stream over/underflow detected." << std::endl;
-
-  // get pointers to uninterlaced input and output frames for each channel.
-  const float* inputs[kInputChannels]{};
-  float* outputs[kOutputChannels]{};
-  for (int i = 0; i<kInputChannels; ++i)
-  {
-    inputs[i] = pInputBuffer + i*nBufferFrames;
-  }
-  for (int i = 0; i<kOutputChannels; ++i)
-  {
-    outputs[i] = pOutputBuffer + i*nBufferFrames;
-  }
-
-  processBuffer.process(inputs, outputs, nBufferFrames,
-                        [&](const DSPVectorArray<2> inputVectors)
-                        {
-                          return processVectors(inputVectors);
-                        } );
-  return 0;
+  return append(vTapL, vTapR);
 }
 
 int main( int argc, char *argv[] )
 {
-  RtAudio adac;
-
-  if ( adac.getDeviceCount() < 1 )
-  {
-    std::cout << "\nNo audio devices found!\n";
-    exit( 1 );
-  }
-  else
-  {
-    RtAudio::DeviceInfo info;
-    uint32_t devices = adac.getDeviceCount();
-
-    std::cout << "[rtaudio] Found: " << devices << " device(s)\n";
-
-    for (uint32_t i = 0; i < devices; ++i)
-    {
-      info = adac.getDeviceInfo(i);
-      std::cout << "\tDevice: " << i << " - " << info.name << std::endl;
-    }
-    std::cout << std::endl;
-  }
-
-  // Let RtAudio print messages to stderr.
-  adac.showWarnings( true );
-
-  // Set the same number of channels for both input and output.
-  unsigned int bufferFrames = 512;
-  RtAudio::StreamParameters iParams, oParams;
-  iParams.deviceId = 0;// adac.getDefaultInputDevice();
-  iParams.nChannels = 0;// kInputChannels;
-  iParams.firstChannel = 0;
-  oParams.deviceId = adac.getDefaultOutputDevice();
-  oParams.nChannels = kOutputChannels;
-  oParams.firstChannel = 0;
-
-  RtAudio::StreamOptions options;
-  options.flags |= RTAUDIO_NONINTERLEAVED;
-
+  using processFnType = std::function<DSPVectorArray<kOutputChannels>(const DSPVectorArray<kInputChannels>&)>;
 
   initializeReverb();
 
-  try
-  {
-    adac.openStream( &oParams, /*&iParams*/ 0, RTAUDIO_FLOAT32, kSampleRate, &bufferFrames, &callProcessVectorsBuffered, nullptr, &options );
-  }
-  catch ( RtAudioError& e )
-  {
-    std::cout << '\n' << e.getMessage() << '\n' << std::endl;
+  processFnType processFn([&](const DSPVectorArray<kInputChannels> inputVectors) { return processVectors(inputVectors); });
 
-#ifdef _WINDOWS
-    system("pause");
-#endif
-    exit( 1 );
-  }
-
-  // Test RtAudio functionality for reporting latency.
-  std::cout << "\nStream latency = " << adac.getStreamLatency() << " frames" << std::endl;
-
-  try
-  {
-    adac.startStream();
-
-    char input;
-    std::cout << "\nRunning ... press <enter> to quit (buffer frames = " << bufferFrames << ").\n";
-    std::cin.get(input);
-
-    adac.stopStream();
-  }
-  catch ( RtAudioError& e )
-  {
-    std::cout << '\n' << e.getMessage() << '\n' << std::endl;
-    goto cleanup;
-  }
-
-cleanup:
-  if ( adac.isStreamOpen() ) adac.closeStream();
-#ifdef _WINDOWS
-  system("pause");
-#endif
-  return 0;
+  return RunRtAudioExample(kInputChannels, kOutputChannels, kSampleRate, &callProcessVectorsBuffered<kInputChannels, kOutputChannels>, &processFn);
 }
