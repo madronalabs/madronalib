735911ce81f0b4550d5aa38205dacce978f8f3cc
Randy Jones <randy@madronalabs.com>
2021-03-29 14:21:55 -0700

    added new modules

---
+++ source/app/MLActor.h
@@ -0,0 +1,58 @@
+// madronalib: a C++ framework for DSP applications.
+// Copyright (c) 2019 Madrona Labs LLC. http://www.madronalabs.com
+// Distributed under the MIT license: http://madrona-labs.mit-license.org/
+
+#pragma once
+
+#include "MLCollection.h"
+#include "MLParameter.h"
+
+namespace ml{
+
+// actor can have state machine functions using PropertyTree as storage.
+
+class Actor : public Collectable
+{
+  static constexpr size_t kMessageQueueSize = 16;
+  Queue< Message > _messageQueue{ kMessageQueueSize };
+  Timer _queueTimer;
+  
+protected:
+  size_t getMessagesAvailable() { return _messageQueue.elementsAvailable(); }
+  
+public:
+  // We can think of an Actor, mainly, as a message handler.
+  virtual void handleMessage(Message m) = 0;
+  
+  Actor(){}
+  virtual ~Actor() = default;
+  
+  void start()
+  {
+    _queueTimer.start([=](){ handleMessages(); }, milliseconds(1000/60));
+  }
+  
+  void stop()
+  {
+    _queueTimer.stop();
+  }
+
+  // Collectable
+  inline void recv(Message m)
+  {
+    // TEMP todo error check
+    auto ok = _messageQueue.push(m);
+  }
+  
+  inline void handleMessages()
+  {
+    Message m;
+    while(auto notEmpty = _messageQueue.pop(m))
+    {
+      handleMessage(m);
+    }
+  }
+};
+
+
+} // namespace ml
