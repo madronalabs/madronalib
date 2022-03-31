de352aa2d43597bb15758b1ed828458252450f51
Randy Jones <randy@mac-studio.lan>
2022-03-30 21:50:31 -0700

    fix compile warning on MacOS 12

--- source/app/MLPlatform.h
+++ source/app/MLPlatform.h
@@ -9,7 +9,7 @@
 #define ML_WINDOWS 1
 #elif defined(LINUX) || defined(__linux__)
 #define ML_LINUX 1
-#elif (TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR)
+#elif defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR)
 #define ML_IOS 1
 #elif defined(__APPLE__)
 #define ML_MAC 1
