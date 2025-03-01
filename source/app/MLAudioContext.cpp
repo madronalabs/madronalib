48e5dc59928a3850a544b436d6f2af6c9bc3dab7
Randy Jones <randy@madronalabs.com>
2025-03-01 13:34:42 -0800

    refactor

---
+++ source/app/MLAudioContext.cpp
@@ -0,0 +1,112 @@
+//
+// Created by Randy Jones on 2/27/25.
+//
+
+#include "MLAudioContext.h"
+
+namespace ml
+{
+
+// AudioContext::ProcessTime
+
+// Set the time and bpm. The time refers to the start of the current processing block.
+// This should be called every task processing block with the latest info.
+
+void AudioContext::ProcessTime::setTimeAndRate(const double ppqPos, const double bpmIn,
+  bool isPlaying, double sampleRateIn)
+{
+  // working around a bug I can't reproduce, so I'm covering all the bases.
+  if (((ml::isNaN(ppqPos)) || (ml::isInfinite(ppqPos))) ||
+      ((ml::isNaN(bpmIn)) || (ml::isInfinite(bpmIn))) )
+  {
+    // debug << "PluginProcessor::ProcessTime::setTimeAndRate: bad input! \n";
+    return;
+  }
+
+  sampleRate = sampleRateIn;
+  bpm = bpmIn;
+  bool active = (_ppqPos1 != ppqPos) && isPlaying;
+  bool justStarted = isPlaying && !_playing1;
+
+  double ppqPhase = 0.;
+  if(active)
+  {
+    if (ppqPos > 0.f)
+    {
+      ppqPhase = ppqPos - floor(ppqPos);
+    }
+    else
+    {
+      ppqPhase = ppqPos;
+    }
+
+    _omega = ppqPhase;
+
+    if (justStarted)
+    {
+      // just start at 0 and don't attempt to match the playhead position.
+      // this works well when we start at any 1/4 note.
+      // there is still some weirdness when we try to lock onto other 16ths.
+      samplesSinceStart = 0;
+      _omega = 0.;
+      double dsdt = (1. / sampleRate);
+      double minutesPerSample = dsdt/60.;
+      _dpdt = bpm*minutesPerSample;
+    }
+    else
+    {
+      double dPhase = ppqPhase - _ppqPhase1;
+      if (dPhase < 0.)
+      {
+        dPhase += 1.;
+      }
+      _dpdt = ml::clamp(dPhase / static_cast<double>(_samplesSincePreviousTime), 0., 1.);
+    }
+  }
+  else
+  {
+    _omega = -1.;
+    _dpdt = 0.;
+  }
+
+  _ppqPos1 = ppqPos;
+  _ppqPhase1 = ppqPhase;
+  _active1 = active;
+  _playing1 = isPlaying;
+  _samplesSincePreviousTime = 0;
+}
+
+void AudioContext::ProcessTime::clear(void)
+{
+  _dpdt = 0.;
+  _active1 = false;
+  _playing1 = false;
+}
+
+// generate phasors from the input parameters
+void AudioContext::ProcessTime::process()
+{
+  for (int n = 0; n < kFloatsPerDSPVector; ++n)
+  {
+    _quarterNotesPhase[n] = _omega;
+    _omega += _dpdt;
+    if (_omega > 1.f)
+    {
+      _omega -= 1.f;
+    }
+  }
+  _samplesSincePreviousTime += kFloatsPerDSPVector;
+  samplesSinceStart += kFloatsPerDSPVector;
+}
+
+AudioContext::AudioContext(size_t nInputs, size_t nOutputs, int sr) :
+  inputs(nInputs), outputs(nOutputs), eventsToSignals(sr), sampleRate(sr)
+{}
+
+void AudioContext::processVector()
+{
+  currentTime.process();
+  eventsToSignals.process();
+}
+
+} // ml
---
+++ source/app/MLAudioContext.h
@@ -0,0 +1,78 @@
+//
+// Created by Randy Jones on 2/27/25.
+//
+
+#pragma once
+
+
+#include "MLDSPOps.h"
+#include "MLEventsToSignals.h"
+
+
+#include <cstdlib>
+#include <functional>
+
+using namespace ml;
+namespace ml
+{
+
+// AudioContext: where our signal processors meet the rest of the world.
+// an AudioContext defines the sample rate and provides audio and event I/O.
+
+
+using MainInputs = const DSPVectorDynamic&;
+using MainOutputs = DSPVectorDynamic&;
+
+class AudioContext final {
+public:
+
+  // AudioContext::ProcessTime maintains the current time in a DSP process and can track
+  // the time in the host application if there is one.
+  class ProcessTime
+  {
+  public:
+    ProcessTime() = default;
+    ~ProcessTime() = default;
+
+    // Set the time and bpm. The time refers to the start of the current engine processing block.
+    void setTimeAndRate(const double ppqPos, const double bpmIn, bool isPlaying, double sampleRateIn);
+
+    // clear state
+    void clear();
+
+    // generate phasors from the input parameters
+    void process();
+
+    // phase signal to read TODO rename
+    DSPVector _quarterNotesPhase;
+
+    double bpm{0};
+    double sampleRate{0};
+    uint64_t samplesSinceStart{0};
+
+  private:
+    float _omega{0};
+    bool _playing1{false};
+    bool _active1{false};
+    double _dpdt{0};
+    size_t _samplesSincePreviousTime{0};
+    double _ppqPos1{-1.};
+    double _ppqPhase1{0};
+  };
+
+  AudioContext(size_t nInputs, size_t nOutputs, int sr);
+  ~AudioContext() = default;
+
+  // update everything needed to create a new vector of context signals.
+  void processVector();
+
+  ProcessTime currentTime;
+  ml::EventsToSignals eventsToSignals;
+
+  DSPVectorDynamic inputs;
+  DSPVectorDynamic outputs;
+  int sampleRate{0};
+};
+
+
+} // ml
